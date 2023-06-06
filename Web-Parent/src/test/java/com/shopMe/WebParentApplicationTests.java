4266e0e5c147bc52af9b8fc3a33b8b574660f158
Harshaa <harshaakumar2002@gmail.com>
2023-06-06 16:17:51 +0530

    Created the admin module

---
+++ Web-Parent/src/test/java/com/shopMe/WebParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
